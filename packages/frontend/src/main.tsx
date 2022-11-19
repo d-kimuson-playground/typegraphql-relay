5bdcf6f8e9ecfc171f505b4c64703fd8a100260d
kaito <kaito@mfac.jp>
2022-11-19 09:29:38 +0900

    feat(frontend): switch to createRoot API from ReactDOM.render

--- packages/frontend/src/main.tsx
+++ packages/frontend/src/main.tsx
@@ -1,10 +1,15 @@
-import React from "react"
-import ReactDOM from "react-dom"
+import { StrictMode } from "react"
 import { App } from "./app"
+import { createRoot } from 'react-dom/client';
 
-ReactDOM.render(
-  <React.StrictMode>
+const container = document.getElementById('root');
+if (container === null) {
+  throw new Error("root element not found.")
+}
+
+const root = createRoot(container);
+root.render(
+  <StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById("root")
-)
+  </StrictMode>,
+);
